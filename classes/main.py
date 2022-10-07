f53f290303f890209c628cdaa8219e3ea00849dc
hartleyleo <78177307+hartleyleo@users.noreply.github.com>
2022-10-07 16:18:18 +0800

    Update main.py

--- classes/main.py
+++ classes/main.py
@@ -14,9 +14,11 @@ class MyWindow(arcade.Window):
         super().__init__( SCREEN_WIDTH, SCREEN_HEIGHT, 'Wordle Clone' )
              
 def main():
+    # Creates a window object to display states onto
     window = MyWindow()
     window.center_window()
     
+    # Creates our playstate object and displays it onto the window
     play_state = PlayState( word_list, choosable_word_list )
     play_state.setup()
     window.show_view( play_state )
